1422806282b1c9c8fc4f0dc07b06ea0e70f25367
Lin-Yuting <141534811+Lin-Yuting@users.noreply.github.com>
2023-09-24 03:25:42 +0000

    avance

--- Contexto/context.py
+++ Contexto/context.py
@@ -1,25 +1,69 @@
 import streamlit as st
 
-# st.markdown{
-#     <style>
-#     <
-# }
-st.title("Context")
-st.markdown("<h1 style='font-family: Arial; font-size: 10px;'>Context</h1>", unsafe_allow_html=True)
+st.set_page_config(
+    page_title = "Contexto",
+    layout = "wide",
+    initial_sidebar_state = "expanded"
+)
+
+st.markdown("<h1 style='font-family: Braggadocio; font-size: 36px; text-align: center;'>Contexto</h1>", unsafe_allow_html=True)
 st.header("Noticias")
 
-st.write("Noticia original")
+st.write("Página de entrada")
 
 
-st.sidebar.button("Deportes")
+if st.sidebar.button("Deportes"):
+    st.image("https://media.datacenterdynamics.com/media/images/Extra.original.jpg")
 st.sidebar.button("Arte")
 st.sidebar.button("Ciencia")
 st.sidebar.button("Política")
 st.sidebar.button("Tecnología")
 if st.sidebar.button("Entretenimiento"):
-    st.write("1-")
-    st.write("¡Atención, amantes de las celebridades! Parece que la estrella del pop internacional, Luna Star, está en medio de un misterioso romance que ha dejado a todos boquiabiertos. ¿La persona afortunada? Nada menos que el famoso chef gourmet, Marco Delicioso.")
-    st.write("Los rumores comenzaron cuando Luna y Marco fueron vistos compartiendo una cena íntima en uno de los restaurantes más exclusivos de la ciudad. Testigos aseguran que la química entre ellos era evidente y que pasaron horas riendo y disfrutando de la comida gourmet.")
-    st.write("Pero eso no es todo, algunos paparazzi astutos lograron capturarlos en una escapada secreta a una paradisíaca isla tropical. Parece que están decididos a mantener su relación alejada de los reflectores, ¡pero las fotos no mienten!")
-    st.write("¿Será este el comienzo de una relación seria o simplemente un romance pasajero? ¡Estaremos atentos para traerte todos los detalles de este intrigante affaire de la alta sociedad!")
-    st.write("Recuerda que esto es puramente ficticio y en tono de entretenimiento, ¡no corresponde a una noticia real!")   
+    
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.image("https://media.datacenterdynamics.com/media/images/Extra.original.jpg")
+        linkDeNoticia = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> 10 Noticias que cambiaron el mundo </a>
+        """
+        st.markdown(linkDeNoticia,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
+    with col2:
+        st.image("https://cnnespanol.cnn.com/wp-content/uploads/2023/08/GettyImages-1627311232-e1692541636956.jpg?quality=100&strip=info")
+        linkDeNoticia2 = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> España vence a Inglaterra </a>
+        """
+        st.markdown(linkDeNoticia2,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
+    with col3:
+        st.image("https://imagenes.elpais.com/resizer/RmQ4R6zkGe3on3ezyvO12i6kZBs=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/NPQ3SK5STVDQBMUYSGT6SKBN7Q.jpg")
+        linkDeNoticia3 = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> Conciero de Taylor Swift </a>
+        """
+        st.markdown(linkDeNoticia3,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
+    
+    st.text("--------------------------------------------------------------------------------------------------------------")
+
+    col4, col5, col6 = st.columns(3)
+    with col4:
+        st.image("https://media.datacenterdynamics.com/media/images/Extra.original.jpg")
+        linkDeNoticia4 = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> 10 Noticias que cambiaron el mundo </a>
+        """
+        st.markdown(linkDeNoticia4,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
+    with col5:
+        st.image("https://cnnespanol.cnn.com/wp-content/uploads/2023/08/GettyImages-1627311232-e1692541636956.jpg?quality=100&strip=info")
+        linkDeNoticia5 = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> España vence a Inglaterra </a>
+        """
+        st.markdown(linkDeNoticia5,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
+    with col6:
+        st.image("https://imagenes.elpais.com/resizer/RmQ4R6zkGe3on3ezyvO12i6kZBs=/1200x0/cloudfront-eu-central-1.images.arcpublishing.com/prisa/NPQ3SK5STVDQBMUYSGT6SKBN7Q.jpg")
+        linkDeNoticia6 = """
+            <a href="https://www.youtube.com/?bp=wgUCEAE%3D" style = "font-family: 'Arial',sans-serif; font-size: 24px; text-align: center; display: block;"> Conciero de Taylor Swift </a>
+        """
+        st.markdown(linkDeNoticia6,unsafe_allow_html=True)
+        st.markdown("<h1 style='font-family: Arial; font-size: 12px; text-align: center;'>ooooooooooooooooooooooooooooooooooooo</h1>", unsafe_allow_html=True)
